29eb866d4dd2c390bb31ed445080cc99c9985db6
Herbert Greenlee <greenlee@fnal.gov>
2021-07-23 13:20:00 -0500

    Fix compilation errors.

--- larpandora/LArPandoraEventBuilding/LArPandoraExternalEventBuilding_module.cc
+++ larpandora/LArPandoraEventBuilding/LArPandoraExternalEventBuilding_module.cc
@@ -7,7 +7,6 @@
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
-#include "art/Framework/Services/Optional/TFileService.h"
 #include "art/Utilities/make_tool.h"
 
 #include "canvas/Utilities/InputTag.h"

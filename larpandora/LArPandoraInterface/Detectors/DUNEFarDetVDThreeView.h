1cba46abd92d03fbe16d75c54bcc754d7b44831d
Dom Brailsford <dominicbrailsford@gmail.com>
2021-09-28 06:41:07 -0500

    Start implementation for wire pitch

--- larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView.h
+++ larpandora/LArPandoraInterface/Detectors/DUNEFarDetVDThreeView.h
@@ -10,7 +10,7 @@ namespace lar_pandora{
         geo::View_t TargetViewU(const geo::TPCID::TPCID_t tpc, const geo::CryostatID::CryostatID_t cstat) const override;
         geo::View_t TargetViewV(const geo::TPCID::TPCID_t tpc, const geo::CryostatID::CryostatID_t cstat) const override;
         geo::View_t TargetViewW(const geo::TPCID::TPCID_t tpc, const geo::CryostatID::CryostatID_t cstat) const override;
-        float WirePitch(const geo::View_t view) const override {return 0.f; };
+        float WirePitch(const geo::View_t view) const override;
         float WireAngle(const geo::View_t view, const int tpc, const int cstat) const override {return 0.f; };
         bool ShouldSwitchUV(const unsigned int tpc, const unsigned int cstat) const override {return false; };
         void LoadDetectorGaps(LArDetectorGapList& listOfGaps) override {return; }; 
@@ -31,6 +31,11 @@ namespace lar_pandora{
     inline geo::View_t DUNEFarDetVDThreeView::TargetViewW(const geo::TPCID::TPCID_t tpc, const geo::CryostatID::CryostatID_t cstat) const
     {
         return m_LArSoftGeometry->View(geo::PlaneID(cstat, tpc, 2));
+    }
+
+    inline float DUNEFarDetVDThreeView::WirePitch(const geo::View_t view) const
+    {
+        return m_LArSoftGeometry->WirePitch(view);
     }
 
 }

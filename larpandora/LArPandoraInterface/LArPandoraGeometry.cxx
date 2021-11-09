5c43e0e201efcc3a59ea411c23c6c7c785c576ae
Dom Brailsford <dominicbrailsford@gmail.com>
2021-11-09 07:52:04 -0600

    Implement the daughter gap loading

--- larpandora/LArPandoraInterface/LArPandoraGeometry.cxx
+++ larpandora/LArPandoraInterface/LArPandoraGeometry.cxx
@@ -34,7 +34,7 @@ namespace lar_pandora {
 
     // ATTN: Expectations here are that the input geometry corresponds to either a single or dual phase LArTPC.
     art::ServiceHandle<geo::Geometry const> theGeometry;
-    const bool isDualPhase(theGeometry->MaxPlanes() == 2);
+    //const bool isDualPhase(theGeometry->MaxPlanes() == 2);
     //REPLACEMENT
     LArPandoraDetectorType *detType(GetDetectorType());
 
@@ -104,55 +104,8 @@ namespace lar_pandora {
             listOfGaps.emplace_back(detType->CreateDetectorGap(point1, point2, widths));
         }
       }
-      if (isDualPhase) {
-        for (LArDaughterDriftVolumeList::const_iterator
-               iterDghtr1 = driftVolume1.GetTpcVolumeList().begin(),
-               iterDghtrEnd1 = driftVolume1.GetTpcVolumeList().end();
-             iterDghtr1 != iterDghtrEnd1;
-             ++iterDghtr1) {
-          const LArDaughterDriftVolume& tpcVolume1(*iterDghtr1);
 
-          for (LArDaughterDriftVolumeList::const_iterator
-                 iterDghtr2 = iterDghtr1,
-                 iterDghtrEnd2 = driftVolume1.GetTpcVolumeList().end();
-               iterDghtr2 != iterDghtrEnd2;
-               ++iterDghtr2) {
-            const LArDaughterDriftVolume& tpcVolume2(*iterDghtr2);
-
-            if (tpcVolume1.GetTpc() == tpcVolume2.GetTpc()) continue;
-
-            const float maxDisplacement(LArDetectorGap::GetMaxGapSize());
-
-            const float deltaY(std::fabs(tpcVolume1.GetCenterY() - tpcVolume2.GetCenterY()));
-            const float deltaZ(std::fabs(tpcVolume1.GetCenterZ() - tpcVolume2.GetCenterZ()));
-
-            const float widthY(0.5f * (tpcVolume1.GetWidthY() + tpcVolume2.GetWidthY()));
-            const float widthZ(0.5f * (tpcVolume1.GetWidthZ() + tpcVolume2.GetWidthZ()));
-
-            const float gapY(deltaY - widthY);
-            const float gapZ(deltaZ - widthZ);
-
-            const float X1((tpcVolume1.GetCenterX() < tpcVolume2.GetCenterX()) ?
-                             (tpcVolume1.GetCenterX() + 0.5f * tpcVolume1.GetWidthX()) :
-                             (tpcVolume2.GetCenterX() + 0.5f * tpcVolume2.GetWidthX()));
-            const float X2((tpcVolume1.GetCenterX() > tpcVolume2.GetCenterX()) ?
-                             (tpcVolume1.GetCenterX() - 0.5f * tpcVolume1.GetWidthX()) :
-                             (tpcVolume2.GetCenterX() - 0.5f * tpcVolume2.GetWidthX()));
-            const float Y1(std::min((tpcVolume1.GetCenterY() - 0.5f * tpcVolume1.GetWidthY()),
-                                    (tpcVolume2.GetCenterY() - 0.5f * tpcVolume2.GetWidthY())));
-            const float Y2(std::max((tpcVolume1.GetCenterY() + 0.5f * tpcVolume1.GetWidthY()),
-                                    (tpcVolume2.GetCenterY() + 0.5f * tpcVolume2.GetWidthY())));
-            const float Z1(std::min((tpcVolume1.GetCenterZ() - 0.5f * tpcVolume1.GetWidthZ()),
-                                    (tpcVolume2.GetCenterZ() - 0.5f * tpcVolume2.GetWidthZ())));
-            const float Z2(std::max((tpcVolume1.GetCenterZ() + 0.5f * tpcVolume1.GetWidthZ()),
-                                    (tpcVolume2.GetCenterZ() + 0.5f * tpcVolume2.GetWidthZ())));
-
-            if (std::fabs(gapY) > maxDisplacement || std::fabs(gapZ) > maxDisplacement)
-              listOfGaps.emplace_back(
-                LArDetectorGap(X1, Y1 + widthY, Z1 + widthZ, X2, Y2 - widthY, Z2 - widthZ));
-          }
-        }
-      }
+      detType->LoadDaughterDetectorGaps(driftVolume1, LArDetectorGap::GetMaxGapSize(), listOfGaps);
     }
   }
 
